d7f9c05734d631e634d25a815bb2c7121c0f7cdf
strafari <punpum1@yandex.ru>
2023-12-24 14:50:52 +0700

    Добавил 5 лабу, исправил 4

--- students/Antoshkin_Nikita/laba4/modules/ajax.js
+++ students/Antoshkin_Nikita/laba4/modules/ajax.js
@@ -1,21 +1,16 @@
 class Ajax {
     post(url, callback) {
-        fetch(url, {
-            method: 'POST'
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+        let xhr = new XMLHttpRequest()
+        xhr.open('POST', getUrl(url))
+        xhr.send()
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                const data = JSON.parse(xhr.response)
+                callback(data)
             }
-            return response.json();
-        })
-        .then(data => {
-            callback(data);
-        })
-        .catch(error => {
-            // Обработка ошибок при запросе
-            console.error('There has been a problem with your fetch operation:', error);
-        });
+        }
     }
 }
+
 export const ajax = new Ajax();
