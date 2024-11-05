d8f24fb97cb8f268468c0171200b6f2978652258
Acer <erik03483@gmail.com>
2024-11-05 22:56:54 +0700

    были добавлены файлы лабораторной работы 3

---
+++ students/is22/Baynazarov_Erik_Ilgizovich/Lr3/Laba3/components/button/index.js
@@ -0,0 +1,10 @@
+export class ButtonComponent {
+    constructor(parent) {
+        this.parent = parent;
+    }
+
+    render() {
+        this.parent.insertAdjacentHTML('beforeend',
+            '<button type="button" class="btn btn-primary">Hello, World 4!</button>')
+    }
+}
