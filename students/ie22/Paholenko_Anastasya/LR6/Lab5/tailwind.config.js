166af2f8525448bd39eb74fe4c4e1fbe28b43605
Anastasya Paholenko <nastya1pakh>
2025-01-14 18:42:48 +0700

    Add:LR6

---
+++ students/ie22/Paholenko_Anastasya/LR6/Lab5/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html', 
+    './src/**/*.{js,jsx,ts,tsx}', 
+    './pages/**/*.{html,js}', 
+    './components/**/*.{html,js}', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
